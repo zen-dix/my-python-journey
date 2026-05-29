cd7209fee40d4d86c941913c29cfd228e2f50c1b
zendix <2013alievgleb@gmail.com>
2026-05-29 09:43:46 +0300

    I fix it

--- days/day_27/task1.py
+++ days/day_27/task1.py
@@ -1,1 +1,5 @@
-print("Oops, I broke it")
+from task2 import settings
+import json
+setting = settings()
+with open("setting.json", "w", encoding="utf-8") as file:
+    json.dump(setting, file, ensure_ascii=False, indent=4)
