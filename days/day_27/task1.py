f252c5144338648c5a16ba53cbb524e791e52141
zendix <2013alievgleb@gmail.com>
2026-05-29 09:36:15 +0300

    I broke it

--- days/day_27/task1.py
+++ days/day_27/task1.py
@@ -1,5 +1,1 @@
-from task2 import settings
-import json
-setting = settings()
-with open("setting.json", "w", encoding="utf-8") as file:
-    json.dump(setting, file, ensure_ascii=False, indent=4)
+print("Oops, I broke it")
